f99d8f60f5fe5b17bb51f22572154e6ea5bd844c
Viraj Bhartiya <vlbhartiya@gmail.com>
2025-03-15 12:47:25 +0530

    feat: implement context timeout for wallet balance and stake info retrieval

--- cmd/cli/balance.go
+++ cmd/cli/balance.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"context"
+	"time"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 
@@ -18,6 +20,9 @@ import (
 
 func RunBalance() {
 	log := gologger.Get().With().Str("component", "balance").Logger()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
 	cfg, err := config.LoadConfig("config/config.yaml")
 	if err != nil {
@@ -42,38 +47,33 @@ func RunBalance() {
 		log.Fatal().Err(err).Msg("No private key found - please authenticate first using 'parity auth'")
 	}
 
-	client, err := walletsdk.NewClient(walletsdk.ClientConfig{
-		RPCURL:     cfg.Ethereum.RPC,
-		ChainID:    cfg.Ethereum.ChainID,
-		PrivateKey: common.Bytes2Hex(crypto.FromECDSA(privateKey)),
-	})
+	clientConfig := walletsdk.ClientConfig{
+		RPCURL:       cfg.Ethereum.RPC,
+		ChainID:      cfg.Ethereum.ChainID,
+		PrivateKey:   common.Bytes2Hex(crypto.FromECDSA(privateKey)),
+		TokenAddress: common.HexToAddress(cfg.Ethereum.TokenAddress),
+		StakeAddress: common.HexToAddress(cfg.Ethereum.StakeWalletAddress),
+	}
+
+	client, err := walletsdk.NewClient(clientConfig)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to create Ethereum client")
 	}
 
-	tokenAddr := common.HexToAddress(cfg.Ethereum.TokenAddress)
-	stakeWalletAddr := common.HexToAddress(cfg.Ethereum.StakeWalletAddress)
-
-	token, err := walletsdk.NewParityToken(tokenAddr, client)
+	walletBalance, err := client.GetBalance(client.Address())
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to create token contract")
-	}
-
-	balance, err := token.BalanceOf(&bind.CallOpts{}, client.Address())
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to check token balance")
+		select {
+		case <-ctx.Done():
+			log.Fatal().Err(ctx.Err()).Msg("Operation timed out while getting wallet balance")
+		default:
+			log.Fatal().Err(err).Msg("Failed to get wallet balance")
+		}
 	}
 
 	log.Info().
-		Str("address", client.Address().Hex()).
-		Str("balance", balance.String()).
-		Str("token_address", tokenAddr.Hex()).
-		Msg("Token balance")
-
-	stakeWallet, err := walletsdk.NewStakeWallet(client, tokenAddr, stakeWalletAddr)
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to create stake wallet")
-	}
+		Str("wallet_address", client.Address().Hex()).
+		Str("balance", walletBalance.String()+" PRTY").
+		Msg("Wallet token balance")
 
 	deviceIDManager := deviceid.NewManager(deviceid.Config{})
 	deviceID, err := deviceIDManager.VerifyDeviceID()
@@ -81,16 +81,36 @@ func RunBalance() {
 		log.Fatal().Err(err).Msg("Failed to get device ID")
 	}
 
-	stakeInfo, err := stakeWallet.GetStakeInfo(deviceID)
+	stakeInfo, err := client.GetStakeInfo(deviceID)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to get stake info")
+		select {
+		case <-ctx.Done():
+			log.Fatal().Err(ctx.Err()).Msg("Operation timed out while getting stake info")
+		default:
+			log.Fatal().Err(err).Msg("Failed to get stake info")
+		}
 	}
 
 	if stakeInfo.Exists {
 		log.Info().
 			Str("amount", stakeInfo.Amount.String()+" PRTY").
 			Str("device_id", stakeInfo.DeviceID).
+			Str("wallet_address", stakeInfo.WalletAddress.Hex()).
 			Msg("Current stake info")
+
+		contractBalance, err := client.GetBalance(clientConfig.StakeAddress)
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				log.Fatal().Err(ctx.Err()).Msg("Operation timed out while getting contract balance")
+			default:
+				log.Fatal().Err(err).Msg("Failed to get contract balance")
+			}
+		}
+		log.Info().
+			Str("balance", contractBalance.String()).
+			Str("contract_address", clientConfig.StakeAddress.Hex()).
+			Msg("Contract token balance")
 	} else {
 		log.Info().Msg("No active stake found")
 	}
